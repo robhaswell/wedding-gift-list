6febc6226bb1615389853b4724a05eaef3e4e3fa
Rob Haswell <me@robhaswell.co.uk>
2018-10-03 09:40:07 +0100

    Example carousel.

--- carousel.py
+++ carousel.py
@@ -2,6 +2,7 @@
 """
 Generate carousel HTML.
 """
+import datetime
 import os.path
 
 import oyaml as yaml
@@ -14,7 +15,24 @@ def main():
     with open("carousel.yaml") as fp:
         piclist = yaml.load(fp)
 
-        print(piclist)
+    altered = False
+    for basename, entry in piclist.items():
+        try:
+            desc = entry['desc']
+        except (TypeError, KeyError):
+            path = os.path.join("img", "house", basename + ".jpg")
+            mtime = datetime.datetime.fromtimestamp(os.path.getmtime(path))
+            entry = dict(
+                desc=entry,
+                date=mtime
+            )
+            piclist[basename] = entry
+            altered = True
+
+    if altered:
+        with open("carousel.yaml", "w") as fp:
+            output = yaml.dump(piclist)
+            fp.write(output)
 
 
 if __name__ == "__main__":
